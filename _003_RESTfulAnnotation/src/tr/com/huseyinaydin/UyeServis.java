002725b8ee3dbf114ddf9d99e255781df56377a5
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-06-19 13:09:31 +0300

    RESTful Web Service dipnotlar ile.

---
+++ _003_RESTfulAnnotation/src/tr/com/huseyinaydin/UyeServis.java
@@ -0,0 +1,34 @@
+package tr.com.huseyinaydin;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+//  http://localhost:8080/_003_RESTfulAnnotation/rest/uye/giris
+
+//بسم الله الرحمن الرحيم
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category RESTful Web Services
+ *
+ */
+
+@Path("/uye")
+public class UyeServis {
+
+	@POST
+	@Path(value = "/giris")
+	@Produces("text/html;charset=utf-8")
+	public Response girisYap(@FormParam("kullaniciAdi") String kullaniciAdi) {
+		System.out.println("girisYap formdan gelen parametre: " + kullaniciAdi);
+		String sonuc = "<h1>" + kullaniciAdi + "</h1>";
+		return Response.status(200).entity(sonuc).build();
+	}
+}
