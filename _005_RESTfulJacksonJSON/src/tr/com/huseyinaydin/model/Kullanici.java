b18de67ffdc028ab36c5d02f4b4e2f2986ea3b83
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-06-19 13:10:37 +0300

    RESTful Web Servisleri Jackson kütüphanesi ile JSON.

---
+++ _005_RESTfulJacksonJSON/src/tr/com/huseyinaydin/model/Kullanici.java
@@ -0,0 +1,55 @@
+package tr.com.huseyinaydin.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+//بسم الله الرحمن الرحيم
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category RESTful Web Services
+ *
+ */
+
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name = "kullanici")
+public class Kullanici {
+
+	// @XmlAttribute
+	@XmlElement(name = "id")
+	int kullaniciId;
+
+	@XmlElement(name = "adi")
+	String kullaniciAdi;
+
+	@XmlElement(name = "soyadi")
+	String kullaniciSoyadi;
+
+	public int getKullaniciId() {
+		return kullaniciId;
+	}
+
+	public void setKullaniciId(int kullaniciId) {
+		this.kullaniciId = kullaniciId;
+	}
+
+	public String getKullaniciAdi() {
+		return kullaniciAdi;
+	}
+
+	public void setKullaniciAdi(String kullaniciAdi) {
+		this.kullaniciAdi = kullaniciAdi;
+	}
+
+	public String getKullaniciSoyadi() {
+		return kullaniciSoyadi;
+	}
+
+	public void setKullaniciSoyadi(String kullaniciSoyadi) {
+		this.kullaniciSoyadi = kullaniciSoyadi;
+	}
+}
